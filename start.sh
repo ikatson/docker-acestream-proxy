acf3c4ef7b41f4d107fb4e6994d67201fe3eff60
Igor Katson <igor.katson@gmail.com>
2014-03-18 19:20:21 +0000

    Updating to support newer version

--- start.sh
+++ start.sh
@@ -3,12 +3,11 @@
 TTV_URL="$1"
 
 if [[ -n "$TTV_URL" ]]; then
-    cat > /home/tv/aceproxy-master/plugins/ttvplaylist_config.py << EOF
+    cat > /home/tv/aceproxy-master/plugins/config/torrenttv.py << EOF
 url = '$1'
-host = False
 EOF
     echo "Paste this URL into your player"
-    echo "http://CHANGE_THIS_TO_SERVER_IP:8000/ttvplaylist/ttvplaylist.m3u"
+    echo "http://CHANGE_THIS_TO_SERVER_IP:8000/torrenttv/torrenttv.m3u"
 fi
 
 sed -i 's/vlcuse = False/vlcuse = True/' /home/tv/aceproxy-master/aceconfig.py
